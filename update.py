e1c7fbfbc1506c1114954dd2b67b31801accf59d
Mustafa Dogan <msdogan@ucdavis.edu>
2026-02-18 13:44:18 +0300

    adding perturbed hydrology for CMIP6 scenarios

--- update.py
+++ update.py
@@ -247,87 +247,87 @@ THIS MODULE WILL UPDATE AG PENALTIES AND AG TARGET DELIVERIES
 # save_ts_data(target_cap_ag,os.sep+'UBT.csv',start_date='1921-09-30',end_date='2015-10-31')
 # print('*********************   *********************')
 
-# method 2:
-# demand areas to be updated
-demand_area = {
-    "hu101":"cvpm01",
-    "hu102":"cvpm02",
-    "hu103a":"cvpm03a",
-    "hu103b":"cvpm03b",
-    "hu104":"cvpm04",
-    "hu202":"cvpm05",
-    "hu203":"cvpm06",
-    "hu204":"cvpm07",
-    "hu207":"cvpm08",
-    "hu206":"cvpm09",
-    "hu303":"cvpm10",
-    "hu302":"cvpm11",
-    "hu305":"cvpm12",
-    "hu306":"cvpm13",
-    "hu402a":"cvpm14a",
-    "hu402b":"cvpm14b",
-    "hu404a":"cvpm15a",
-    "hu404b":"cvpm15b",
-    "hu401":"cvpm16",
-    "hu403":"cvpm17",
-    "hu405":"cvpm18",
-    "hu408a":"cvpm19a",
-    "hu408b":"cvpm19b",
-    "hu407":"cvpm20",
-    "hu409a":"cvpm21a",
-    "hu409b":"cvpm21b",
-    "hu409c":"cvpm21c"
-    }
+# # method 2:
+# # demand areas to be updated
+# demand_area = {
+#     "hu101":"cvpm01",
+#     "hu102":"cvpm02",
+#     "hu103a":"cvpm03a",
+#     "hu103b":"cvpm03b",
+#     "hu104":"cvpm04",
+#     "hu202":"cvpm05",
+#     "hu203":"cvpm06",
+#     "hu204":"cvpm07",
+#     "hu207":"cvpm08",
+#     "hu206":"cvpm09",
+#     "hu303":"cvpm10",
+#     "hu302":"cvpm11",
+#     "hu305":"cvpm12",
+#     "hu306":"cvpm13",
+#     "hu402a":"cvpm14a",
+#     "hu402b":"cvpm14b",
+#     "hu404a":"cvpm15a",
+#     "hu404b":"cvpm15b",
+#     "hu401":"cvpm16",
+#     "hu403":"cvpm17",
+#     "hu405":"cvpm18",
+#     "hu408a":"cvpm19a",
+#     "hu408b":"cvpm19b",
+#     "hu407":"cvpm20",
+#     "hu409a":"cvpm21a",
+#     "hu409b":"cvpm21b",
+#     "hu409c":"cvpm21c"
+#     }
 
-return_type = {"GW":"g","SW":"s"}
+# return_type = {"GW":"g","SW":"s"}
 
-months = {'January':"JAN",'February':"FEB",'March':"MAR",'April':"APR",'May':"MAY",'June':"JUN",'July':"JUL",'August':"AUG",'September':"SEP",'October':"OCT",'November':"NOV",'December':"DEC"}
+# months = {'January':"JAN",'February':"FEB",'March':"MAR",'April':"APR",'May':"MAY",'June':"JUN",'July':"JUL",'August':"AUG",'September':"SEP",'October':"OCT",'November':"NOV",'December':"DEC"}
 
-start_date='1921-10-31'
-end_date='2015-09-30'
-date_range = pd.date_range(start=start_date,end=end_date,freq='M')
-target_delivery=pd.DataFrame(index=date_range)
-target_delivery.index.name = 'date'
+# start_date='1921-10-31'
+# end_date='2015-09-30'
+# date_range = pd.date_range(start=start_date,end=end_date,freq='M')
+# target_delivery=pd.DataFrame(index=date_range)
+# target_delivery.index.name = 'date'
 
-# read penalties - raw data
-# Read data
-df = pd.read_csv("data/ag_penalty/penalties_lambdawaters_monthly.csv")
+# # read penalties - raw data
+# # Read data
+# df = pd.read_csv("data/ag_penalty/penalties_lambdawaters_monthly.csv")
 
-unique_hu = df["hu"].unique()
-unique_month = df["month_name"].unique()
-unique_water_type = df["water_type"].unique()
+# unique_hu = df["hu"].unique()
+# unique_month = df["month_name"].unique()
+# unique_water_type = df["water_type"].unique()
 
-for hu in unique_hu:
-    df_region = df[df["hu"] == hu]
-    for wt in unique_water_type:
-        subregion = demand_area[hu]
-        link = hu + '-' + subregion + return_type[wt]
-        penalty_loc = loc_finder(link, directory)+os.sep+'costs'+os.sep
-        print(penalty_loc)
-        df_wt = df_region[df_region["water_type"] == wt]
-        monthly_target = {}
-        for month in unique_month:
-            df_save = pd.DataFrame()
-            print(f'{hu}, {wt}, {month}')
-            df_month = df_wt[df_wt["month_name"] == month]
-            delivery = df_month["xwatersc_monthly_TAF"]
-            penalty = df_month["penalty"]
+# for hu in unique_hu:
+#     df_region = df[df["hu"] == hu]
+#     for wt in unique_water_type:
+#         subregion = demand_area[hu]
+#         link = hu + '-' + subregion + return_type[wt]
+#         penalty_loc = loc_finder(link, directory)+os.sep+'costs'+os.sep
+#         print(penalty_loc)
+#         df_wt = df_region[df_region["water_type"] == wt]
+#         monthly_target = {}
+#         for month in unique_month:
+#             df_save = pd.DataFrame()
+#             print(f'{hu}, {wt}, {month}')
+#             df_month = df_wt[df_wt["month_name"] == month]
+#             delivery = df_month["xwatersc_monthly_TAF"]
+#             penalty = df_month["penalty"]
             
-            if sum(delivery) == 0: # these are months/regions with zero target capacity
-                delivery = [0,1000000] # if you don't update this, problems can arise when calcualting slopes (unit cost c)
-                penalty = [0,0]
+#             if sum(delivery) == 0: # these are months/regions with zero target capacity
+#                 delivery = [0,1000000] # if you don't update this, problems can arise when calcualting slopes (unit cost c)
+#                 penalty = [0,0]
             
-            df_save["capacity"] = delivery
-            df_save["cost"] = penalty
+#             df_save["capacity"] = delivery
+#             df_save["cost"] = penalty
             
-            # save and update penalties
-            df_save.to_csv(penalty_loc+os.sep+months[month]+'.csv', index=False)
+#             # save and update penalties
+#             df_save.to_csv(penalty_loc+os.sep+months[month]+'.csv', index=False)
             
-            monthly_target[month]=df_save['capacity'].iloc[-1]
-        targets=[]
-        for j in target_delivery.index:
-            targets.append(monthly_target[j.strftime("%B")])
-        target_delivery[link]=targets
+#             monthly_target[month]=df_save['capacity'].iloc[-1]
+#         targets=[]
+#         for j in target_delivery.index:
+#             targets.append(monthly_target[j.strftime("%B")])
+#         target_delivery[link]=targets
 
 # # save target capacities    
 # target_delivery.to_csv('data/ubt_ag_target_new.csv')
@@ -341,6 +341,81 @@ for hu in unique_hu:
 # # this will match and update calvin-network-data
 # save_ts_data(target_cap_ag,os.sep+'UBT.csv',start_date='1921-09-30',end_date='2015-10-31')
 # print('*********************   *********************')
+
+# climate scenario
+scenarios = [
+            'ACCESS-CM2', # ssp370 does not exist
+            # 'EC-Earth3-Veg',
+            # 'FGOALS-g3',
+            # 'GFDL-ESM4',
+            # 'INM-CM5-0',
+            # 'IPSL-CM6A-LR',
+            # 'KACE-1-0-G',
+            # 'MIROC6',
+            # 'MPI-ESM1-2-HR',
+            # 'MRI-ESM2-0',
+            # 'TaiESM1' # ssp585 does not exist
+            ]
+# ssp scenario
+ssps = [
+        # 'ssp245',
+        # 'ssp370',
+        'ssp585'
+        ]
+
+# following scenarios do not exist in the database
+skip_pairs = {
+    ('ACCESS-CM2', 'ssp370'),
+    ('TaiESM1', 'ssp585')
+}
+
+# organize projected runoff
+for scenario in scenarios:
+    for ssp in ssps:
+        if (scenario, ssp) in skip_pairs:
+            continue
+        print(f'scenario: {scenario}, ssp: {ssp}')
+        """
+        RIM INFLOWS
+        """
+        print('Updating Reservoir Inflow')
+        # read rim inflow locations (nodes) and time-series data
+        rim_inflows = pd.read_csv(f'data/perturbed_hydrology/rim_inflow/{scenario}_{ssp}_rim_inflow_perturbed.csv', header=0, index_col = 0)
+        # convert index to date time index
+        rim_inflows.index = pd.to_datetime(rim_inflows.index)
+        
+        # this will match and update calvin-network-data
+        save_ts_data(rim_inflows,os.sep+'inflows'+os.sep+'default.csv',start_date='1921-09-30',end_date='2015-10-31')
+        # print('*********************   *********************')
+        
+        
+        """
+        GROUNDWATER INFLOWS
+        """
+        print('Updating Groundwater Inflow')
+        # read groundwater inflow locations (nodes) and time-series data
+        gw_inflows = pd.read_csv(f'data/perturbed_hydrology/gw_inflow/{scenario}_{ssp}_gw_inflow_perturbed.csv', header=0, index_col = 0)
+        # convert index to date time index
+        gw_inflows.index = pd.to_datetime(gw_inflows.index)
+        
+        # this will match and update calvin-network-data
+        save_ts_data(gw_inflows,os.sep+'inflows'+os.sep+'default.csv',start_date='1921-09-30',end_date='2015-10-31')
+        print('*********************   *********************')
+        
+        
+        """
+        LOCAL INFLOWS AND LOSSES
+        """
+        print('Updating Local Inflows')
+        # read local inflow and loss locations (nodes) and time-series data
+        local_flow = pd.read_csv(f'data/perturbed_hydrology/local_inflow/{scenario}_{ssp}_local_inflow_perturbed.csv', header=0, index_col = 0)
+        # convert index to date time index
+        local_flow.index = pd.to_datetime(local_flow.index)
+        
+        # this will match and update calvin-network-data
+        save_ts_data(local_flow,os.sep+'EQT.csv',start_date='1921-09-30',end_date='2015-10-31')
+        print('*********************   *********************')
+
 
 
 '''
@@ -364,3 +439,5 @@ NOT WORKING YET - THIS MODULE WILL MODIFY NODE.GEOJSON files - NOT WORKING YET
 #             json_object["properties"]["flow"]=None
 #             with open(filepath, "w") as outfile:
 #                 json.dump(json_object, outfile)
+
+print('%%%%%%%%  FINISHED  %%%%%%%%')
